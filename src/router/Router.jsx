e8e920bfa852dbd28c9bd42dfce1c85ce27396b0
Augui Maxime <auguimaxime.pro@gmail.com>
2026-05-26 14:57:41 +0200

    Update Router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,7 +3,7 @@ import Layout from '../pages/Layout';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Logement from '../pages/Logement';
-import NotFound from '../pages/Notfound';
+import NotFound from '../pages/NotFound';
 
 const Router = () => (
   <Routes>
